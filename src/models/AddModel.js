460824abae0c5eb6f162aa0b9d5871d70e436d64
unknown <webdesign1954@hotmail.com>
2023-01-31 17:19:08 +0100

    fixed - axios

--- src/models/AddModel.js
+++ src/models/AddModel.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import Constants from '../utilities/Constants';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Constants from "../utilities/Constants";
 
 export default function AddModel() {
-   //loading
+  //loading
   const [searchModel, setSearchModel] = useState("");
-  const [brandId, setBrandId] = useState('');
+  const [brandId, setBrandId] = useState("");
   const apiGetBrandsEndPoint = Constants.API_URL_GET_ALL_BRANDS;
 
-  useEffect(() => {   
+  useEffect(() => {
     loadBrands();
   }, []);
   const [brands, setBrands] = useState([]);
@@ -18,70 +18,65 @@ export default function AddModel() {
     setBrands(result.data);
   };
 
-//post
+  //post
   let navigate = useNavigate();
   const [brand, setBrand] = useState({
     name: "",
-     });
-  const {name} = brand
-  const onInputChange =(e)=>{
-    setBrand({...brand,[e.target.name]:e.target.value})
-  }
-const onSubmit = async(e)=>{
-e.preventDefault();
-await axios.post(`http://localhost:8080/brands/${brandId}/model`, brand);
-navigate("/")
-
-
-  }
-  const handleBrand =(event)=>{
-    const getBrandId=event.target.value;    
+  });
+  const { name } = brand;
+  const onInputChange = (e) => {
+    setBrand({ ...brand, [e.target.name]: e.target.value });
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post(`http://localhost:8080/brands/${brandId}/model`, brand);
+    navigate("/");
+  };
+  const handleBrand = (event) => {
+    const getBrandId = event.target.value;
     setBrandId(getBrandId);
-   
   };
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">Add Model </h2>
-<form onSubmit={(e)=>onSubmit(e)}>
-          <div className="md-3">
-            <label htmlFor="FirstName" className="form-label">
-              Model name
-            </label>
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className="md-3">
+              <label htmlFor="FirstName" className="form-label">
+                Model name
+              </label>
 
-            <select
-          name="Brands"
-          className="form-control"
-          /* handleBrand */ onChange={(e) => handleBrand(e)}
-        >
-          <option value="">--Select Brand--</option>
-          {brands.map((getBrand) => (
-            <option key={getBrand.id} value={getBrand.id}>
-              {getBrand.name}
-            </option>
-          ))}
-        </select>
+              <select
+                name="Brands"
+                className="form-control"
+                /* handleBrand */ onChange={(e) => handleBrand(e)}
+              >
+                <option value="">--Select Brand--</option>
+                {brands.map((getBrand) => (
+                  <option key={getBrand.id} value={getBrand.id}>
+                    {getBrand.name}
+                  </option>
+                ))}
+              </select>
 
-            <input
-              type={"text"}
-              className="form-control"
-              placeholder="Enter model name"
-              name="name"
-              value={name}
-             onChange={(e)=>onInputChange(e)}
-            ></input>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter model name"
+                name="name"
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              ></input>
+            </div>
 
- 
-          </div>
-
-<br/>
-          <button type="submit" className="btn btn-outline-primary">
-            Submit
-          </button>
-          <Link to="/" className="btn btn-outline-danger">
-            Cancel
-          </Link>
+            <br />
+            <button type="submit" className="btn btn-outline-primary">
+              Submit
+            </button>
+            <Link to="/" className="btn btn-outline-danger">
+              Cancel
+            </Link>
           </form>
         </div>
       </div>
